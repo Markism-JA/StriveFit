4fe869c66e566816586298cf4b7fdfe45b424175
marky <markism1224amarille@gmail.com>
2025-05-23 00:33:42 +0800

    Health Connect permission

---
+++ app/src/main/java/com/marky/strivefit/permissions/HealthConnect.kt
@@ -0,0 +1,12 @@
+package com.marky.strivefit.permissions
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class HealthConnect : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
